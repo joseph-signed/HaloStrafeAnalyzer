b99a6498a40a4778c3317b4c850debde2b9eb5e2
joseph-signed <105678044+joseph-signed@users.noreply.github.com>
2022-08-13 14:53:02 -0500

    Add default window size to app

--- StrafinV2/App.xaml.cs
+++ StrafinV2/App.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace StrafinV2
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+
+namespace StrafinV2
 {
     public partial class App : Application
     {
+        //Sets App Window Size
+        const int WindowWidth = 1000;
+        const int WindowHeight = 600;
+
         public App()
         {
             InitializeComponent();
+
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+                AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+            });
 
             MainPage = new MainPage();
         }
