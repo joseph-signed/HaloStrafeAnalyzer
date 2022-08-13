291fffaca10ccaf68eb16ea5bea7aa46d900e6b6
joseph-signed <105678044+joseph-signed@users.noreply.github.com>
2022-08-13 12:43:07 -0500

    Implement SharpDX.XInput and successfully pull xbox controller joystick values

---
+++ StrafinV2/Models/XInputController.cs
@@ -0,0 +1,40 @@
+﻿using SharpDX.XInput;
+
+
+namespace StrafinV2.Models
+{
+    internal class XInputController
+    {
+        Controller controller;
+        Gamepad gamepad;
+        public bool connected = false;
+        public int deadband = 3200;
+        public float left_x = 0;
+        public float left_y = 0;
+        public float right_x = 0;
+        public float right_y = 0;
+
+        public XInputController()
+        {
+            controller = new Controller(UserIndex.One);
+            connected = controller.IsConnected;
+        }
+
+        // Call this method to update all class values
+        public bool Update()
+        {
+            if (!connected)
+                return false;
+
+            gamepad = controller.GetState().Gamepad;
+
+            left_x = (Math.Abs((float)gamepad.LeftThumbX) < deadband) ? 0 : (float)gamepad.LeftThumbX / short.MaxValue * 100;
+            left_y = (Math.Abs((float)gamepad.LeftThumbY) < deadband) ? 0 : (float)gamepad.LeftThumbY / short.MaxValue * 100;
+
+            right_x = (Math.Abs((float)gamepad.RightThumbX) < deadband) ? 0 : (float)gamepad.RightThumbX / short.MaxValue * 100;
+            right_y = (Math.Abs((float)gamepad.RightThumbY) < deadband) ? 0 : (float)gamepad.RightThumbY / short.MaxValue * 100;
+            return true;
+
+        }
+    }
+}
